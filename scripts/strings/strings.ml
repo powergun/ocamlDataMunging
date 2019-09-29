18a7e2dbb9087913bfbeb880c81c073cc63367e5
Wei Ning <macgnw@gmail.com>
2019-09-29 16:42:39 +1000

    wip

---
+++ scripts/strings/strings.ml
@@ -0,0 +1,14 @@
+#!/usr/bin/env ocaml
+
+let runDemo () = 
+  let c = String.get "Test" 3 in
+    print_char(c);
+  print_endline(String.make 9 'c');
+  print_endline("there is " ^ "a cow");
+  print_endline(String.concat "." ["there"; "is"; "a"; "cow"]);;
+
+  
+  
+
+  ;;
+runDemo();;
